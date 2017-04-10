07c4c4b85cbe66c89dca2986e94e7c49983ef3ea
Leijurv <leijurv@gmail.com>
2017-04-10 09:37:45 -0700

    Update Device.java

--- app/src/main/java/io/getcoffee/ottopress/core/Device.java
+++ app/src/main/java/io/getcoffee/ottopress/core/Device.java
@@ -2,7 +2,7 @@ package io.getcoffee.ottopress.core;
 
 public class Device {
 
-    public String core, modifier;
+    public final String core, modifier;
 
     public Device(String core, String modifier) {
         this.core = core;
